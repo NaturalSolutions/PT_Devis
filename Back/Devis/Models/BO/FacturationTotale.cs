b012a5895cc625200ce50d4dae7d42416b95bff2
Schtroudel <lopez.tom.fr@gmail.com>
2017-12-21 12:58:47 +0100

    ajout de la facturation totale

---
+++ Back/Devis/Models/BO/FacturationTotale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+
+namespace Devis.Models.BO
+{
+    public class FacturationTotale
+    {
+        public List<Facturation> amo { get; set; }
+        public List<Facturation> dev { get; set; }
+        public List<Facturation> des { get; set; }
+
+        public FacturationTotale()
+        {
+
+        }
+
+        public void updateValue()
+        {
+            amo.ForEach(x => x.updateValue(true));
+            dev.ForEach(x => x.updateValue(false));
+            des.ForEach(x => x.updateValue(false));
+        }
+
+    }
+}
