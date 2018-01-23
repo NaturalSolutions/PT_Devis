efe3dae415a5b7b06ea94477fa8c79738bb28025
TomLopez <tom_lopez@natural-solutions.eu>
2018-01-23 10:01:40 +0100

    better sort condition

--- Front/app/modules/init/PT_DataAcess.js
+++ Front/app/modules/init/PT_DataAcess.js
@@ -16,8 +16,8 @@ function getAllProjects() {
 		success: function (data) {
 			console.log('desdatas', data);
 			data.sort(function (a, b) {
-				if (a.name < b.name) return -1;
-				if (a.name > b.name) return 1;
+				if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+				if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
 				return 0;
 			});
 			myProjects = data;
