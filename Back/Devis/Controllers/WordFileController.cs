568339070620bb2dc9203bfc5343608bab5de3af
Schtroudel <lopez.tom.fr@gmail.com>
2018-01-24 12:34:51 +0100

    ajout de la gestion des fichiers word avec template et de la route associé non terminé

---
+++ Back/Devis/Controllers/WordFileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Devis.Models.BO;
+using Newtonsoft.Json;
+
+namespace Devis.Controllers
+{
+    public class WordFileController : ApiController
+    {
+        public object Get()
+        {
+            return new { truc = "truc"};
+        }
+
+        //[ActionName("factu")]
+        //public string GetFactu([FromUri]string initials, [FromUri]int type)
+        //{          
+        //    return JsonConvert.SerializeObject(new Facturation(initials, type), Formatting.Indented);
+        //}
+
+        //[ActionName("get")]
+        //public byte[] GetFactu(Guid id)
+        //{           
+        //    return new byte[0];
+        //}
+
+        [ActionName("create")]
+        public void GetCreate(object docInfos)
+        {
+            WordFile test = new WordFile();
+        }
+    }
+}
---
+++ Back/Devis/Models/BO/WordFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using Xceed.Words.NET;
+
+namespace Devis.Models.BO
+{
+    public class WordFile
+    {
+        DocX final { get; set; }
+
+       public WordFile()
+       {
+            this.final = loadTemplate();
+            DateTime longDate = DateTime.Now;            
+            setDate("dateCreation", longDate);
+            //Save template to a new name same location
+            //TODO : convenir d'une convention de nommage 
+            this.final.SaveAs(@"C:\inetpub\wwwroot\devis\Back\Devis\Content\Devis_" + longDate.ToString().Trim().Replace('/','-').Replace(':','_').Replace(@"\s+", "") + ".docx");
+       }
+
+        private DocX loadTemplate()
+        {
+            string fileName = @"C:\inetpub\wwwroot\devis\Back\Devis\Content\templateDevis.docx";
+            DocX temp = DocX.Load(fileName);
+            //LoadFile in memory
+            return temp;
+        }
+
+        private void setDate(string balise, DateTime toSet)
+        {
+            this.final.ReplaceText("[" + balise + "]", toSet.ToShortDateString());
+        }
+    }
+}
